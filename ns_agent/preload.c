7d6e4ca6eaa5b5d3128342aca386db694f2e7754
zhuzhicheng <1477046511@qq.com>
2021-04-06 11:39:41 +0800

    04_06_1

--- ns_agent/preload.c
+++ ns_agent/preload.c
@@ -8,9 +8,14 @@
 typedef FILE *(*fopen_t)(const char *pathname, const char *mode);
 typedef int (* open_t)(const char *pathname, int flags, ...);
 typedef int (* open64_t)(const char *pathname, int flags, ...);
+typedef int (* openat_t)(int dirfd, const char *pathname, int flags, ...);
+typedef int (* openat64_t)(int dirfd, const char *pathname, int flags, ...);
+
 fopen_t real_fopen;
 open_t real_open;
 open64_t real_open64;
+openat_t real_openat;
+openat64_t real_openat64;
 
 FILE *fopen(const char *pathname, const char *mode) {
   fprintf(stderr, "called fopen(%s, %s)\n", pathname, mode);
@@ -26,7 +31,7 @@ int open64(const char *pathname, int flags, ...) {
   if (!real_open) {
     real_open64 = dlsym(RTLD_NEXT, "open64");
   }
-  if(flags & O_CREAT) {
+  if(__OPEN_NEEDS_MODE(flags)) {
     va_list v;
     va_start(v, flags);
     mode_t mode;
@@ -41,7 +46,7 @@ int open(const char *pathname, int flags, ...) {
   if (!real_open) {
     real_open = dlsym(RTLD_NEXT, "open");
   }
-  if(flags & O_CREAT) {
+  if(__OPEN_NEEDS_MODE(flags)) {
     va_list v;
     va_start(v, flags);
     mode_t mode;
@@ -49,6 +54,36 @@ int open(const char *pathname, int flags, ...) {
     return real_open(pathname, flags, mode); 
   }
   else return real_open(pathname, flags);
+}
+
+int openat(int dirfd, const char *pathname, int flags, ...) {
+  fprintf(stderr, "called openat(%s)\n", pathname);
+  if (!real_openat) {
+    real_openat = dlsym(RTLD_NEXT, "openat");
+  }
+  if(__OPEN_NEEDS_MODE(flags)) {
+    va_list v;
+    va_start(v, flags);
+    mode_t mode;
+    mode = va_arg(v, mode_t);
+    return real_openat(dirfd, pathname, flags, mode); 
+  }
+  else return real_openat(dirfd, pathname, flags);
+}
+
+int openat64(int dirfd, const char *pathname, int flags, ...) {
+  fprintf(stderr, "called openat64(%s)\n", pathname);
+  if (!real_openat64) {
+    real_openat64 = dlsym(RTLD_NEXT, "openat64");
+  }
+  if(__OPEN_NEEDS_MODE(flags)) {
+    va_list v;
+    va_start(v, flags);
+    mode_t mode;
+    mode = va_arg(v, mode_t);
+    return real_openat64(dirfd, pathname, flags, mode); 
+  }
+  else return real_openat64(dirfd, pathname, flags);
 }
 
 __attribute__((constructor)) static void setup(void) {
