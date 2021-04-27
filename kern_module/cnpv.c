e988b6bd89cd16eafe161e0fb8f93c069521ab4c
zhuzhicheng <1477046511@qq.com>
2021-04-27 13:46:40 +0800

    04_27_2

--- kern_module/cnpv.c
+++ kern_module/cnpv.c
@@ -1,18 +1,85 @@
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/pid.h>
+#include "cnpv.h"
 
-static int __init myled_init(void) {
-  printk("myled drvier init !\n");
-  return 0;
-}
-
-static int __exit myled_exit(void) {
-  printk("myled drvier exit !\n");
-  return 0;
-}
-
-module_init(myled_init);
-module_exit(myled_exit);
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hook sys_call_open by change sys_open entry");
+
+/* The sys_call_table is read-only => must make it RW before replacing a syscall
+ */
+void set_addr_rw(unsigned long addr) {
+
+	unsigned int level;
+	pte_t *pte = lookup_address(addr, &level);
+
+	if (pte->pte &~ _PAGE_RW) pte->pte |= _PAGE_RW;
+
+}
+
+/* Restores the sys_call_table as read-only */
+void set_addr_ro(unsigned long addr) {
+
+	unsigned int level;
+	pte_t *pte = lookup_address(addr, &level);
+
+	pte->pte = pte->pte &~_PAGE_RW;
+
+}
+
+unsigned long **acquire_syscall_table(void) {
+  return (unsigned long **)kallsyms_lookup_name("sys_call_table");
+}
+
+unsigned long copied;
+
+asmlinkage int my_open(const char __user *pathname, int flags, mode_t mode) {
+  int (*real_open)(const char __user *, int, mode_t) =
+      (int (*)(const char __user *, int, mode_t))sys_open_ptr;
+
+  char user_msg[256];
+  memset(user_msg, 0, sizeof(user_msg));
+  copied = strncpy_from_user(user_msg, pathname, sizeof(user_msg));
+  printk("%s\n", __FUNCTION__);
+  // printk("copied:%ld\n", copied);
+  printk("pathname%s\n", user_msg);
+  printk("------\n");
+
+  return (*real_open)(pathname, flags, mode);
+}
+
+static int __init open_hook_init(void) {
+  unsigned long **syscall_table = acquire_syscall_table();
+
+  // disable_page_protection();
+  set_addr_rw((unsigned long)syscall_table);
+
+  // now replace the syscal
+  STORE_SYSCALLPTR(syscall_table, open);
+  REPLACE_SYSCALL(syscall_table, open);
+
+  // enable_page_protection();
+  set_addr_ro((unsigned long)syscall_table);
+
+  printk(KERN_INFO "starting intercept\n");
+
+  return 0;
+}
+
+static void __exit open_hook_exit(void) {
+  void **syscall_table = (void **)acquire_syscall_table();
+
+  // disable_page_protection();
+  set_addr_rw((unsigned long)syscall_table);
+
+  // now recover the syscall
+  RECOVER_SYSCALLPTR(syscall_table, open);
+
+  // enable_page_protection();
+  set_addr_ro((unsigned long)syscall_table);
+
+  printk(KERN_INFO "Bye bye open_hook\n");
+
+  return;
+}
+
+module_init(open_hook_init);
+
+module_exit(open_hook_exit);
--- kern_module/cnpv.h
+++ kern_module/cnpv.h
@@ -2,40 +2,38 @@
 #define _SYSCALL_INTERCEPT_H_
 
 #include <linux/init.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/syscalls.h>
 // #include <linux/pid>
 #include <linux/unistd.h>
 
-#define STORE_SYSCALLPTR(table, name) sys_ ## name ## _ptr = (void *)table[__NR_ ## name]
-#define REPLACE_SYSCALL(table, name) table[__NR_ ## name] = (void *)my_ ## name
-#define RECOVER_SYSCALLPTR(table, name) table[__NR_ ## name] = sys_ ## name ## _pt
+#define STORE_SYSCALLPTR(table, name) sys_##name##_ptr = (void *)table[__NR_##name]
+#define REPLACE_SYSCALL(table, name) table[__NR_##name] = (void *)my_##name
+#define RECOVER_SYSCALLPTR(table, name) table[__NR_##name] = sys_##name##_ptr
 
 // INFO: ``fn`` is short for ``__func__``.
-# define fn_printk(level, fmt, ...)                     \
+#define fn_printk(level, fmt, ...) \
     printk(level "%s: " fmt, __func__, ##__VA_ARGS__)
 
 // INFO: ``fm`` is short for ``__func__`` and ``module``.
-# define fm_printk(level, fmt, ...)                     \
-    printk(level "%s.%s: " fmt,                        \
+#define fm_printk(level, fmt, ...) \
+    printk(level "%s.%s: " fmt,    \
            THIS_MODULE->name, __func__, ##__VA_ARGS__)
 
 // INFO: I only use ``pr_alert`` at present.
 // TODO: When wanting more, e.g. ``pr_info``, I will add them.
-# define fn_alert(fmt, ...) \
+#define fn_alert(fmt, ...) \
     fn_printk(KERN_ALERT, fmt, ##__VA_ARGS__)
 
-# define fm_alert(fmt, ...) \
+#define fm_alert(fmt, ...) \
     fm_printk(KERN_ALERT, fmt, ##__VA_ARGS__)
-
 
 void *sys_open_ptr;
 void *sys_openat_ptr;
-
-asmlinkage long
-my_open(const char __user *filename, int flags, umode_t mode)
+asmlinkage int 
+my_open(const char __user *pathname, int flags, mode_t mode);
 
 unsigned long **
 acquire_syscall_table(void);
