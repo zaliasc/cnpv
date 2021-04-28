860a359b2132083b02db8f947e099f96887dc4ba
zhuzhicheng <1477046511@qq.com>
2021-04-28 09:59:45 +0800

    04_28_4

--- kern_module/utils.c
+++ kern_module/utils.c
@@ -74,44 +74,7 @@ char *load_file(char *filename, int *filesize) {
   memset(buf, 0, size);
   struct file *f = file_open(filename, O_RDONLY, 0);
   file_read(f, 0, buf, size); 
-
-  // while (size > 0) {
-  //   if (size >= 512) {
-  //     file_read(f, record, buf + record, 512);
-  //     size -= 512;
-  //     record += 512;
-  //   } else {
-  //     file_read(f, record, buf + record, size);
-  //   }
-  //   printk("size: %d record: %d", size, record);
-  // }
-  printk("size : %d \nbuf : %s",size, buf);
+  printk("read file size : %d", size;
   file_close(f);
   return buf;
 }
-
-// static void read_file(char *filename) {
-//   struct file *f = NULL;
-//   int fd;
-//   char buf[1];
-//   loff_t f_pos = 0;
-//   mm_segment_t old_fs = get_fs();
-//   tab = kmalloc(15, GFP_KERNEL);
-//   set_fs(KERNEL_DS);
-
-//   f = filp_open(filename, O_RDONLY, 0);
-//   if (!IS_ERR(f)) {
-//     printk(KERN_DEBUG);
-//     while (kernel_read(f, buf, 1, &f_pos) == 1) {
-//       printk("%c", buf[0]);
-//       if (buf[0] != '\n') {
-//         tab[i] = buf[0];
-//         printk("tab[%d] = %c", i, tab[i]);
-//         i++;
-//       }
-//     }
-//     printk("\n");
-//     filp_close(f, NULL);
-//   }
-//   set_fs(old_fs);
-// }
