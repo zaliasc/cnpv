47327b3b5ac00f4403807bb4710e020a241991d1
zhuzhicheng <1477046511@qq.com>
2021-04-28 10:47:46 +0800

    04_28_6 netlinktest

--- netlink_test/nluser.c
+++ netlink_test/nluser.c
@@ -1,17 +1,22 @@
+#include <fcntl.h>
 #include <linux/netlink.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define NETLINK_USER 31
 
-#define MAX_PAYLOAD 1024 /* maximum payload size*/
+#define MAX_PAYLOAD 1024 * 16 /* maximum payload size*/
 struct sockaddr_nl src_addr, dest_addr;
 struct nlmsghdr *nlh = NULL;
 struct iovec iov;
 int sock_fd;
 struct msghdr msg;
+
+char config_path[] = "/home/zhuzhicheng/project/cnpv/ns_agent/config.json";
 
 int main() {
   sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
@@ -36,7 +41,36 @@ int main() {
   nlh->nlmsg_pid = getpid();
   nlh->nlmsg_flags = 0;
 
-  strcpy(NLMSG_DATA(nlh), "Hello");
+  int fd;
+  struct stat filestatus;
+  int file_size;
+  char *file_contents;
+
+  if (stat(config_path, &filestatus) != 0) {
+    printf("File %s not found", config_path);
+    exit(-1);
+  }
+
+  file_size = filestatus.st_size;
+  file_contents = (char *)malloc(filestatus.st_size);
+  if (file_contents == NULL) {
+    printf("Memory error: unable to allocate %d bytes", file_size);
+    exit(-1);
+  }
+
+  fd = open(config_path, O_RDONLY);
+  if (fd == 0) {
+    free(file_contents);
+    exit(-1);
+  }
+  if (read(fd, file_contents, file_size) == -1) {
+    printf("read failed");
+    free(file_contents);
+    exit(-1);
+  }
+  close(fd);
+
+  strcpy(NLMSG_DATA(nlh), file_contents);
 
   iov.iov_base = (void *)nlh;
   iov.iov_len = nlh->nlmsg_len;
