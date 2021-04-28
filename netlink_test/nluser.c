34fe2967469db6d691d80db06fd24fb0c563fd3d
zhuzhicheng <1477046511@qq.com>
2021-04-28 11:21:42 +0800

    04_28_8 netlinktest

--- netlink_test/nluser.c
+++ netlink_test/nluser.c
@@ -1,3 +1,5 @@
+#include "json.h"
+#include "log.h"
 #include <fcntl.h>
 #include <linux/netlink.h>
 #include <stdio.h>
@@ -9,7 +11,7 @@
 
 #define NETLINK_USER 31
 
-#define MAX_PAYLOAD 1024 * 16 /* maximum payload size*/
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
 struct sockaddr_nl src_addr, dest_addr;
 struct nlmsghdr *nlh = NULL;
 struct iovec iov;
@@ -18,29 +20,7 @@ struct msghdr msg;
 
 char config_path[] = "/home/zhuzhicheng/project/cnpv/ns_agent/config.json";
 
-int main() {
-  sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
-  if (sock_fd < 0)
-    return -1;
-
-  memset(&src_addr, 0, sizeof(src_addr));
-  src_addr.nl_family = AF_NETLINK;
-  src_addr.nl_pid = getpid(); /* self pid */
-
-  bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
-
-  memset(&dest_addr, 0, sizeof(dest_addr));
-  memset(&dest_addr, 0, sizeof(dest_addr));
-  dest_addr.nl_family = AF_NETLINK;
-  dest_addr.nl_pid = 0;    /* For Linux Kernel */
-  dest_addr.nl_groups = 0; /* unicast */
-
-  nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
-  memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
-  nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-  nlh->nlmsg_pid = getpid();
-  nlh->nlmsg_flags = 0;
-
+char *getfile_content() {
   int fd;
   struct stat filestatus;
   int file_size;
@@ -71,22 +51,131 @@ int main() {
   close(fd);
 
   printf("file_content: %s", file_contents);
+}
 
-  strcpy(NLMSG_DATA(nlh), file_contents);
+// static void process_pair(json_value *pair) {
+//   if (pair->type != json_object) {
+//     log_err("parse pair failed");
+//     exit(-1);
+//   }
 
-  iov.iov_base = (void *)nlh;
-  iov.iov_len = nlh->nlmsg_len;
-  msg.msg_name = (void *)&dest_addr;
-  msg.msg_namelen = sizeof(dest_addr);
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
+//   struct user tmp = {.pathname = {0}};
 
-  printf("Sending message to kernel\n");
-  sendmsg(sock_fd, &msg, 0);
+//   for (int i = 0; i < pair->u.array.length; i++) {
+//     if (!strcmp(pair->u.object.values[i].name, "PATH")) {
+//       strncpy(tmp.pathname, pair->u.object.values[i].value->u.string.ptr,
+//               MAX_PATH - 1);
+//     } else if (!strcmp(pair->u.object.values[i].name, "AUTHORITY")) {
+//       tmp.permission =
+//           get_mode_value(pair->u.object.values[i].value->u.string.ptr);
+//     }
+//   }
+
+//   if (tmp.pathname[strlen(tmp.pathname) - 1] == '/') {
+//     // dir
+//     get_dir_content(tmp.pathname, tmp.permission);
+//   } else {
+//     // hashmap_set(map, &tmp);
+//   }
+// }
+
+// static void process_array(json_value *array) {
+//   if (array->type != json_array) {
+//     log_err("parse array failed");
+//     exit(-1);
+//   }
+
+//   for (int i = 0; i < array->u.array.length; i++) {
+//     process_pair(array->u.array.values[i]);
+//   }
+// }
+
+// void json_process(char *file_contents, int file_size) {
+//   json_char *json;
+//   json_value *value;
+//   json = (json_char *)file_contents;
+//   value = json_parse(json, file_size);
+
+//   if (value == NULL) {
+//     log_err("Unable to parse config_file!");
+//     free(file_contents);
+//     exit(1);
+//   }
+
+//   if (value->type != json_object) {
+//     log_err("parse root failed");
+//     exit(-1);
+//   }
+
+//   for (int i = 0; i < value->u.object.length; i++) {
+//     // if (strstr(value->u.object.values[i].name,
+//     // program_invocation_short_name)) {
+//     process_array(value->u.object.values[i].value);
+//     // }
+//   }
+
+//   return;
+// }
+
+void mysendmsg(const char *str);
+
+int main() {
+  sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+  if (sock_fd < 0)
+    return -1;
+
+  memset(&src_addr, 0, sizeof(src_addr));
+  src_addr.nl_family = AF_NETLINK;
+  src_addr.nl_pid = getpid(); /* self pid */
+
+  bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+
+  memset(&dest_addr, 0, sizeof(dest_addr));
+  memset(&dest_addr, 0, sizeof(dest_addr));
+  dest_addr.nl_family = AF_NETLINK;
+  dest_addr.nl_pid = 0;    /* For Linux Kernel */
+  dest_addr.nl_groups = 0; /* unicast */
+
+  nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+  memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+  nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+  nlh->nlmsg_pid = getpid();
+  nlh->nlmsg_flags = 0;
+
+  char *file_contents = getfile_content();
+
+  // strcpy(NLMSG_DATA(nlh), file_contents);
+
+  // iov.iov_base = (void *)nlh;
+  // iov.iov_len = nlh->nlmsg_len;
+  // msg.msg_name = (void *)&dest_addr;
+  // msg.msg_namelen = sizeof(dest_addr);
+  // msg.msg_iov = &iov;
+  // msg.msg_iovlen = 1;
+
+  // printf("Sending message to kernel\n");
+  // sendmsg(sock_fd, &msg, 0);
+  mysendmsg("111");
+  mysendmsg("222");
+  mysendmsg("333");
+  mysendmsg("444");
+
   printf("Waiting for message from kernel\n");
 
   /* Read message from kernel */
   recvmsg(sock_fd, &msg, 0);
   printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
   close(sock_fd);
+}
+
+void mysendmsg(const char *str) {
+  strcpy(NLMSG_DATA(nlh), str);
+  iov.iov_base = (void *)nlh;
+  iov.iov_len = nlh->nlmsg_len;
+  msg.msg_name = (void *)&dest_addr;
+  msg.msg_namelen = sizeof(dest_addr);
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  printf("Sending message to kernel\n");
+  sendmsg(sock_fd, &msg, 0);
 }
